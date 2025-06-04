6e9ca3bde9f02444004ea3f1d5b60a647cddfe2a
anghel3m <adms95@outlook.es>
2025-06-04 10:17:21 -0500

    pimera plantilla

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,24 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'nosotros',
+    loadComponent: () => import('./pages/nosotros/nosotros.page').then( m => m.NosotrosPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'heroes',
+    loadComponent: () => import('./pages/heroes/heroes.page').then( m => m.HeroesPage)
+  },
+
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-  ],
-});
+    provideIonicAngular({}),
+    provideRouter(routes, withComponentInputBinding())
+  ]
+}).catch(err => console.error(err));
