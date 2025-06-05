28f98c41e848402ba7e620dce050846c34d8e3d6
anghel3m <adms95@outlook.es>
2025-06-04 19:30:49 -0500

    nuevas imagnes

--- src/app/pages/heroes/heroes.page.ts
+++ src/app/pages/heroes/heroes.page.ts
@@ -40,7 +40,7 @@ import { HeroImageModalComponent } from '../../components/hero-image-modal/hero-
   ]
 })
 export class HeroesPage {
- heroes = [
+heroes = [
   { 
     name: "Superman", 
     power: "Fuerza sobrehumana",
@@ -60,8 +60,59 @@ export class HeroesPage {
     name: "Flash", 
     power: "Súper velocidad",
     image: "https://upload.wikimedia.org/wikipedia/en/6/6c/Flash_DC_Comics.png"
+  },
+  {
+    name: "Iron Man",
+    power: "Armadura avanzada y genio inventor",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55.jpg"
+  },
+  {
+    name: "Spider-Man",
+    power: "Sentido arácnido y agilidad",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b.jpg"
+  },
+  {
+    name: "Capitán América",
+    power: "Fuerza mejorada y escudo invencible",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/3/50/537ba56d31087.jpg"
+  },
+  {
+    name: "Thor",
+    power: "Dios del trueno y Mjölnir",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/d/d0/5269657a74350.jpg"
+  },
+  {
+    name: "Hulk",
+    power: "Fuerza ilimitada con ira",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0.jpg"
+  },
+  {
+    name: "Black Widow",
+    power: "Espionaje y combate cuerpo a cuerpo",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/f/30/50fecad1f395b.jpg"
+  },
+  {
+    name: "Doctor Strange",
+    power: "Hechicería y manipulación del tiempo",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/5/f0/5261a85a501fe.jpg"
+  },
+  {
+    name: "Pantera Negra",
+    power: "Tecnología y agilidad felina",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/6/60/5261a80a67e7d.jpg"
+  },
+  {
+    name: "Capitana Marvel",
+    power: "Energía cósmica y vuelo",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/c/10/537ba5ff07aa4.jpg"
+  },
+  {
+    name: "Ant-Man",
+    power: "Cambio de tamaño y comunicación con insectos",
+    image: "https://i.annihil.us/u/prod/marvel/i/mg/6/90/537bc6ae8a5b7.jpg"
   }
 ];
+
 
   constructor(
     private router: Router,
